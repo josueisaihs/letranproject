e38279d2207f54349b04b9ea248064517efd408f
Josué Isai Hernández Sánchez <josueisaihs@gmail.com>
2020-07-30 12:58:42 -0400

    Add files via upload

--- Bartolo/settings.py
+++ Bartolo/settings.py
@@ -122,11 +122,10 @@ USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_server')
-STATICFILES_DIRS = ('static', 'static_local')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static', 'static_local'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
