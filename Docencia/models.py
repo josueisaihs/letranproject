386cccf1da855dbf82a4c8b9d2d0ef4dc76f50ac
Josué Isai Hernández Sánchez <josueisaihs@gmail.com>
2020-08-13 11:55:25 -0400

    Add files via upload

---
+++ Docencia/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+from Docencia.DatosPersonales.models import *
+from Docencia.Cursos.models import *
+from Docencia.Admision.models import *
+from Docencia.Index.models import *
