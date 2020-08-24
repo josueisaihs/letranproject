1b004b0e83d06dd9d1003af13820e5994915ebfc
josueisaihs <josueisaihs@gmail.com>
2020-08-24 17:18:26 -0400

    Pues nada manio atras que eso no sirvio

--- static/letran/js/index.js
+++ static/letran/js/index.js
@@ -169,27 +169,15 @@ function filtroCursos(){
 function getPreview(externalUrl){
     var target = externalUrl;
 
-    // $.ajax({
-    //     url: "https://api.linkpreview.net",
-    //     dataType: 'json',
-    //     data: {q: target, key: 'ff55066756cdd0c5f063bd3c7d33c138'},
-    //     success: function (data) {
-    //         $(".enlace-title").html(data.title);
-    //         $(".enlace-url").html(data.url);
-    //         $(".enlace-img").attr("src", data.image);
-    //         $(".enlace-card").fadeIn();
-    //     }
-    // });
-
     $.ajax({
-        url: urls.urlApi,
-        data: {url: target,},
-        success: function(data){
-            console.log(data)
+        url: "https://api.linkpreview.net",
+        dataType: 'json',
+        data: {q: target, key: 'ff55066756cdd0c5f063bd3c7d33c138'},
+        success: function (data) {
             $(".enlace-title").html(data.title);
             $(".enlace-url").html(data.url);
             $(".enlace-img").attr("src", data.image);
-            $(".enlace-card").fadeIn();
+            $(".enlace-card").show("slow");
         }
     });
 }
