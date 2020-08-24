4998941f71907f3295b99653843a16c468786620
josueisaihs <josueisaihs@gmail.com>
2020-08-24 16:07:11 -0400

    creo que ya jajajaja

--- static/letran/js/application.js
+++ static/letran/js/application.js
@@ -30,8 +30,8 @@ $(document).ready(()=>{
         });
 
         asktextareas.map((iter, ask)=>{
-            console.log($(ask), $(ask).minLength, $(ask).val().length >= $(ask).minLength);
-            if ($(ask).val().length >= $(ask).minLength){
+            console.log($(ask), $(ask)[0].minLength, $(ask).val().length >= $(ask)[0].minLength);
+            if ($(ask).val().length >= $(ask)[0].minLength){
                 asks.push([$(ask).attr("data-ask"), "t", $(ask).val()]);
             }else{
                 $(ask).addClass("is-invalid");
