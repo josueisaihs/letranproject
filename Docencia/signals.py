27bb2a4a1c29e2e524d703aa490e19ee170f81bd
josueisaihs <josueisaihs@gmail.com>
2020-09-02 21:03:37 -0400

    añadiendo la signals a los eventos y arreglando la funcion facebook que tenia conflictos con la libreria

--- Docencia/signals.py
+++ Docencia/signals.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 # import locale
 # locale.setlocale(locale.LC_TIME, "es_CU")
 
-from Docencia.tasks import enviar_suscriptores, twitter, facebook
-from Docencia.Index.models import News
+from Docencia.tasks import enviar_suscriptores, twittertweet, facebookposts
+from Docencia.Index.models import News, Events
 
 @receiver(post_save, sender=News)
 def signal_enviar_email(sender, instance, **kwargs):
@@ -19,17 +19,43 @@ def signal_enviar_email(sender, instance, **kwargs):
             instance.date.strftime("%a %d %b %Y %H:%M").title(), 
             schedule=delta
         )
-        
-        twitter(
+
+        twittertweet(
             instance.pk,
             instance.title,
             instance.body[:100],
             schedule=delta
         )
 
-        facebook(
+        facebookposts(
             instance.pk,
             instance.title,
             instance.body[:100],
             schedule=delta
         )
+
+@receiver(post_save, sender=Events)
+def signal_enviar_evento(sender, instance, **kwargs):
+    if (kwargs.get("created", False)):
+        enviar_evento_suscriptores(
+            instance.pk,
+            instance.name,
+            instance.body,
+            schedule=timezone.now()
+        )
+        twittertweet(
+            instance.pk,
+            instance.name,
+            instance.body,
+            'evento',
+            schedule=timezone.now()
+        )
+
+        facebookposts(
+            instance.pk,
+            instance.name,
+            instance.body,
+            'evento',
+            schedule=timezone.now()
+        )
+
