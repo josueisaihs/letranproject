6d9ae08fd838f4b4de19d096e68ab80027590e49
josueisaihs <josueisaihs@gmail.com>
2021-01-05 12:01:52 -0500

    Revert "Merge commit '91ba695956ae6eb824652b57563454fc9d1d2fe9' into sapereaude_init"
    This reverts commit 80d05e15f282eb523020a4426068f76d19f40c1a, reversing
    changes made to ec9f90d4132efedeec8b4c5abfac21a865854df5.

--- Docencia/DatosPersonales/models.py
+++ Docencia/DatosPersonales/models.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.contrib.auth.models import User, Group
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
-from datetime import date
 import os
 from datetime import date
 
