67d52eb796211e25f055b31f827700218968b212
josueisaihs <josueisaihs@gmail.com>
2021-01-05 10:56:52 -0500

    poniendo la edad en la planilla

--- Docencia/DatosPersonales/models.py
+++ Docencia/DatosPersonales/models.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.contrib.auth.models import User, Group
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
+from datetime import date
 import os
 from datetime import date
 
