bf33587cb837fd84f11f44462a311c1c6165b1f2
josueisaihs <josueisaihs@gmail.com>
2021-01-16 05:23:05 -0500

    haciendo cambios

--- static/letran/js/exportPDF.js
+++ static/letran/js/exportPDF.js
@@ -3,7 +3,16 @@
 $(document).ready(function () {
     $('#exportpdf').click((e)=>{
         const source = $(".ckeditor")[0]
+        const classname = $(".ckeditor").attr('data-classname')
 
-        var worker = html2pdf().from(source).save();
+        var opt = {
+            margin:       1,
+            filename:     `${classname}.pdf`,
+            image:        { type: 'jpeg', quality: 0.98 },
+            html2canvas:  { scale: 2 },
+            jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+        };
+
+        var worker = html2pdf().set(opt).from(source).save();
     })
 });
