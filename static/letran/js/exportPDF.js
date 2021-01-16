17b2e440c8686e8157826a98ef9629090096e445
josueisaihs <josueisaihs@gmail.com>
2021-01-16 05:16:26 -0500

    probando con otra libreria

--- static/letran/js/exportPDF.js
+++ static/letran/js/exportPDF.js
@@ -2,32 +2,8 @@
 
 $(document).ready(function () {
     $('#exportpdf').click((e)=>{
-        var pdf = new jsPDF('p', 'pt', 'letter');
         const source = $(".ckeditor")[0]
 
-        const specialElementHandlers = {
-            '#bypassme': function (element, renderer) {
-                return true
-            }
-        };
-        const margins = {
-            top: 80,
-            bottom: 60,
-            left: 40,
-            width: 522
-        };
-
-        pdf.fromHTML(
-            source, 
-            margins.left, // x coord
-            margins.top, { // y coord
-                'width': margins.width, 
-                'elementHandlers': specialElementHandlers
-            },
-
-            function (dispose) {
-                pdf.save('Prueba.pdf');
-            }, margins
-        );
-    });
+        var worker = html2pdf().from(source).save();
+    })
 });
