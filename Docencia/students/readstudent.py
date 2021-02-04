0e631d2e863ce68fe4d133d71c3d1bf182b8e86b
josueisaihs <josueisaihs@gmail.com>
2021-02-03 23:51:32 -0500

    ya casi

--- Docencia/students/readstudent.py
+++ Docencia/students/readstudent.py
@@ -13,7 +13,7 @@ with open(os.path.join(BASE_DIR, 'students', 'Datos estudiantes DEH.csv'), 'r', 
     for line in file.readlines():
         nombre, apellidos, genero, ci, direccion, muncipio, provincia, movil, telefono, email, nacionalidad, ocupacion, grado, titulo = line.split(";")
         try:
-            student = StudentPersonalInformation.objects.filter(name=nombre, lastname=apellidos, email=email)
+            student = StudentPersonalInformation.objects.filter(name=nombre, lastname=apellidos, email=email, numberidentification=ci)
             if student.__len__() > 0:
                 student = student[0]
             else:
