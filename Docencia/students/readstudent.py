3a8d0300563a2474c200b76ba624f13a8b0dd6d3
josueisaihs <josueisaihs@gmail.com>
2021-02-03 23:30:01 -0500

    l

--- Docencia/students/readstudent.py
+++ Docencia/students/readstudent.py
@@ -12,28 +12,31 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 with open(os.path.join(BASE_DIR, 'students', 'Datos estudiantes DEH.csv'), 'r', encoding="ISO-8859-1") as file:
     for line in file.readlines():
         nombre, apellidos, genero, ci, direccion, muncipio, provincia, movil, telefono, email, nacionalidad, ocupacion, grado, titulo = line.split(";")
-        
-        student = StudentPersonalInformation()
-        student.name = nombre
-        student.lastname = apellidos
-        student.genero = ""
-        student.numberidentification = ci
-        student.street = direccion
-        student.city = muncipio
-        student.state = provincia
-        student.phone = telefono
-        student.cellphone = movil
-        student.email = email
-        student.nacionality = nacionalidad
-        student.title = titulo
-        student.save()
+        try:
+            student = StudentPersonalInformation()
+            student.name = nombre
+            student.lastname = apellidos
+            student.genero = ""
+            student.numberidentification = ci
+            student.street = direccion
+            student.city = muncipio
+            student.state = provincia
+            student.phone = telefono
+            student.cellphone = movil
+            student.email = email
+            student.nacionality = nacionalidad
+            student.title = titulo
+            student.save()
 
-        app = Application()
-        app.course = CourseInformation.objects.get(name="Desarrollo Web")
-        app.student = student
-        app.status = "aceptado"
-        app.edition = Edition.objects.get(active=True)
+            app = Application()
+            app.course = CourseInformation.objects.get(name="Desarrollo Web")
+            app.student = student
+            app.status = "aceptado"
+            app.edition = Edition.objects.get(active=True)
 
-        app.save()
+            app.save()
+            print("Listo %s %s" % (nombre, apellidos))
+        except:
+            print("Error con %s %s" % (nombre, apellidos))
 
     file.close()
