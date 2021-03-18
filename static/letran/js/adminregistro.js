4bbff19fe6c4c06ba279b0d2d880d600c54fdd47
josueisaihs <josueisaihs@gmail.com>
2021-03-18 16:58:11 -0400

    boton de check para el registro

--- static/letran/js/adminregistro.js
+++ static/letran/js/adminregistro.js
@@ -1,10 +1,17 @@
 $(document).ready(function () {
     var toastSuccess = new bootstrap.Toast(document.getElementById("id_toast"), 'autohide');
+
+    document.querySelectorAll('.form-check-input').forEach(element=>{
+        $(element).change(e=>{
+            const form_control = $(element).parent().children('.form-control')
+            $(form_control).prop('disabled', !$(element).prop('checked'))            
+        })
+    })
+
     document.querySelectorAll('.form-control').forEach(element => {
         $(element).change((e)=>{
             const nota = $(element).val();
             const pk = $(element).attr('data-enrollment');
-             console.log("Enviando...")
             $.ajax({
                 type: "post",
                 url: urlnote,
