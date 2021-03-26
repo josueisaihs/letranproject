35f94d62b7a9ffe4218f8d26b692cb36a5328b3b
Josué Isai Hernández Sánchez <josueisaihs@JoshuaMac.local>
2021-03-26 15:36:38 -0400

    indicador de que se esta eliminando la tarea

--- static/letran/js/homeworks.js
+++ static/letran/js/homeworks.js
@@ -1,13 +1,21 @@
 $(document).ready(function () {
     document.querySelectorAll('.btn').forEach(element => {
+        
         $(element).click(function (e) { 
-            
+            $(element).prop("disabled", true)
+            let spinner = $(element).find($(".spinner-grow"))[0]
+            $(spinner).removeClass('invisible')
             $.ajax({
                 type: "post",
                 url: urldelete,
                 data: {'slug': $(element).attr('data-slug')},
                 success: function (response) {
                     location.reload()
+                },
+                error: (xhr, errmsg, err)=>{
+                    console.log(err, errmsg);
+                    $(element).prop("disabled", false)
+                    $(spinner).addClass('invisible')
                 }
             });
         });
