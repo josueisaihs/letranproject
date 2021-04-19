086b37b0fdc6e55cf18e149a21ed8c4e5c1e8b36
josueisaihs <josueisaihs@gmail.com>
2021-04-19 14:23:53 -0400

    cambios vista de profesores y buscador para los recursos

--- static/letran/js/recursosearch.js
+++ static/letran/js/recursosearch.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
-    $(".form-control").on("change", ()=>{
+    $(".form-control").on("keydown", ()=>{
         const txt = $(".form-control").val().toString().toLowerCase()
+        console.log(txt)
         document.querySelectorAll(".recourse-container").forEach(el => {
             const name = $(el).attr("data-name")
-            if (name.toString().toLowerCase().indexOf(txt) > -1) 
+            if (name !== ""){
+                if (name.toString().toLowerCase().indexOf(txt) > -1) 
+                    $(el).show()
+                else 
+                    $(el).hide()
+            }else{
                 $(el).show()
-            else 
-                $(el).hide()
+            }
+            
         })
     })
 });
